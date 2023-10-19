ec7bce7e269249cc9a813fe67d0449d88b1f526c
lipo541 <141038060+lipo541@users.noreply.github.com>
2023-10-19 06:38:47 +0400

    zz

--- src/components/productfilter/productfilter.jsx
+++ src/components/productfilter/productfilter.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect  } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './productfilter.css';
 import arrowdown from '../../assets/logo/arrowdown.png';
@@ -8,224 +8,190 @@ import star4 from '../../assets/rating/4star.png';
 import star3 from '../../assets/rating/3star.png';
 import star2 from '../../assets/rating/2star.png';
 
-
 const Productfilter = () => {
-    // make filter 
-    const [products, setProducts] = useState([]);
+  // Make category filter
+  const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-      axios
-        .get('https://amazon-digital-prod.azurewebsites.net/api/product/categories')
-        .then((response) => {
-          setProducts(response.data);
-        })
-        .catch((error) => {
-          console.error('API request error:', error);
-        });
-        
-    }, []);
+  useEffect(() => {
+    axios
+      .get('https://amazon-digital-prod.azurewebsites.net/api/product/categories')
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.error('API request error:', error);
+      });
+  }, []);
 
-    // arrow rotate, hide filter
-  const [isCategoryOpen, setCategoryOpen] = useState(true);
-  const [isBrandsOpen, setBrandsOpen] = useState(true);
-  const [isFeaturesOpen, setFeaturesOpen] = useState(true);
-  const [isPriceRangeOpen, setPriceRangeOpen] = useState(true);
-  const [isConditionOpen, setConditionOpen] = useState(true);
-  const [isRatingOpen, setRatingOpen] = useState(true);
+  // Make brand filter
+  const [brands, setBrands] = useState([]);
 
+  useEffect(() => {
+    axios
+      .get('https://amazon-digital-prod.azurewebsites.net/api/product/products')
+      .then((response) => {
+        setBrands(response.data);
+      })
+      .catch((error) => {
+        console.error('API request error:', error);
+      });
+  }, []);
 
-  const toggleCategory = () => {
-    setCategoryOpen(!isCategoryOpen);
-  };
+  // State for toggle all filters
+  const [isFilterOpen, setFilterOpen] = useState({
+    category: true,
+    brands: true,
+    features: true,
+    priceRange: true,
+    condition: true,
+    rating: true,
+  });
 
-  const toggleBrands = () => {
-    setBrandsOpen(!isBrandsOpen);
-  };
-
-  const toggleFeatures = () => {
-    setFeaturesOpen(!isFeaturesOpen);
-  };
-
-  const togglePriceRange = () => {
-    setPriceRangeOpen(!isPriceRangeOpen);
-  };
-
-  const toggleCondition = () => {
-    setConditionOpen(!isConditionOpen);
-  };
-
-  const toggleRating = () => {
-    setRatingOpen(!isRatingOpen);
+  // Toggle function for all filters
+  const toggleFilter = (filterName) => {
+    setFilterOpen({ ...isFilterOpen, [filterName]: !isFilterOpen[filterName] });
   };
 
   return (
     <div className='navigation_container'>
-        <div className='filter_container'>
-            {/* Category Filter */}
-            <div className={`filter_box ${isCategoryOpen ? 'open' : ''}`}>
-                <div className='nav_head' onClick={toggleCategory}>
-                    <span>Category</span>
-                    <img src={arrowdown} alt="" className={isCategoryOpen ? 'rotate' : ''} />
-                </div>
-                <ul style={{ display: isCategoryOpen ? 'block' : 'none' }}>
-                {products.map((product) => (
-                    <li key={product.id}>{product.name}</li>
-                ))}
-                </ul>
-                {isCategoryOpen && <span className='seeall'>See all</span>}
-            </div>
-
-            {/* Brands Filter */}
-            <div className={`filter_box ${isBrandsOpen ? 'open' : ''}`}>
-                <div className='nav_head' onClick={toggleBrands}>
-                    <span>Brands</span>
-                    <img src={arrowdown} alt="" className={isBrandsOpen ? 'rotate' : ''} />
-                </div>
-                <ul style={{ display: isBrandsOpen ? 'block' : 'none' }}>
-                    <li>
-                        <label>
-                            <input type="checkbox" name="" id="" />Samsung
-                        </label>
-                    </li>
-                    <li>
-                        <label>
-                            <input type="checkbox" name="" id="" />Apple
-                        </label>
-                    </li>
-                    <li>
-                        <label>
-                            <input type="checkbox" name="" id="" />Huawei
-                        </label>
-                    </li>
-                    <li>
-                        <label>
-                            <input type="checkbox" name="" id="" />Pocco
-                        </label>
-                    </li>
-                </ul>
-                {isBrandsOpen && <span className='seeall'>See all</span>}
-            </div>
-
-            {/* Features Filter */}
-            <div className={`filter_box ${isFeaturesOpen ? 'open' : ''}`}>
-                <div className='nav_head' onClick={toggleFeatures}>
-                    <span>Features</span>
-                    <img src={arrowdown} alt="" className={isFeaturesOpen ? 'rotate' : ''} />
-                </div>
-                <ul style={{ display: isFeaturesOpen ? 'block' : 'none' }}>
-                    <li>
-                        <label>
-                            <input type="checkbox" name="" id="" />Metallic
-                        </label>
-                    </li>
-                    <li>
-                        <label>
-                            <input type="checkbox" name="" id="" />Plastic cover
-                        </label>
-                    </li>
-                    <li>
-                        <label>
-                            <input type="checkbox" name="" id="" />8GB Ram
-                        </label>
-                    </li>
-                    <li>
-                        <label>
-                            <input type="checkbox" name="" id="" />Super power
-                        </label>
-                    </li>
-                    <li>
-                        <label>
-                            <input type="checkbox" name="" id="" />Large Memory
-                        </label>
-                    </li>
-                </ul>
-                {isFeaturesOpen && <span className='seeall'>See all</span>}
-            </div>
-
-            {/* Price Range Filter */}
-            <div className={`filter_box ${isPriceRangeOpen ? 'open' : ''}`}>
-                <div className='nav_head' onClick={togglePriceRange}>
-                    <span>Price range</span>
-                    <img src={arrowdown} alt="" className={isPriceRangeOpen ? 'rotate' : ''} />
-                </div>
-                <div className='priserange_filter' style={{ display: isPriceRangeOpen ? 'block' : 'none' }}>
-                    <PriceRangeSlider />
-                </div>
-                {isPriceRangeOpen && <span>See all</span>}
-            </div>
-
-            {/* Condition Filter */}
-            <div className={`filter_box ${isConditionOpen ? 'open' : ''}`}>
-                <div className='nav_head' onClick={toggleCondition}>
-                    <span>Condition</span>
-                    <img src={arrowdown} alt="" className={isConditionOpen ? 'rotate' : ''} />
-                </div>
-                <ul style={{ display: isConditionOpen ? 'block' : 'none' }}>
-                    <li>
-                        <label>
-                            <input type="checkbox" name="" id="" />Metallic
-                        </label>
-                    </li>
-                    <li>
-                        <label>
-                            <input type="checkbox" name="" id="" />Plastic cover
-                        </label>
-                    </li>
-                    <li>
-                        <label>
-                            <input type="checkbox" name="" id="" />8GB Ram
-                        </label>
-                    </li>
-                    <li>
-                        <label>
-                            <input type="checkbox" name="" id="" />Super power
-                        </label>
-                    </li>
-                    <li>
-                        <label>
-                            <input type="checkbox" name="" id="" />Large Memory
-                        </label>
-                    </li>
-                </ul>
-                {isConditionOpen && <span className='seeall'>See all</span>}
-            </div>
-
-            {/* Rating Filter */}
-            <div className={`filter_box ${isRatingOpen ? 'open' : ''}`}>
-                <div className='nav_head' onClick={toggleRating}>
-                    <span>Rating</span>
-                    <img src={arrowdown} alt="" className={isRatingOpen ? 'rotate' : ''} />
-                </div>
-                <ul style={{ display: isRatingOpen ? 'block' : 'none' }}>
-                    <li>
-                        <label>
-                            <input type="checkbox" name="" id="" />
-                            <img src={star5} alt="" />
-                        </label>
-                    </li>
-                    <li>
-                        <label>
-                            <input type="checkbox" name="" id="" />
-                            <img src={star4} alt="" />
-                        </label>
-                    </li>
-                    <li>
-                        <label>
-                            <input type="checkbox" name="" id="" />
-                            <img src={star3} alt="" />
-                        </label>
-                    </li>
-                    <li>
-                        <label>
-                            <input type="checkbox" name="" id="" />
-                            <img src={star2} alt="" />
-                        </label>
-                    </li>
-                </ul>
-            </div>
+      <div className='filter_container'>
+        {/* Category Filter */}
+        <div className={`filter_box ${isFilterOpen.category ? 'open' : ''}`}>
+          <div className='nav_head' onClick={() => toggleFilter('category')}>
+            <span>Category</span>
+            <img src={arrowdown} alt='' className={isFilterOpen.category ? 'rotate' : ''} />
+          </div>
+          <ul style={{ display: isFilterOpen.category ? 'block' : 'none' }}>
+            {products.map((product) => (
+              <li key={product.id}>{product.name}</li>
+            ))}
+          </ul>
+          {isFilterOpen.category && <span className='seeall'>See all</span>}
         </div>
+
+        {/* Brands Filter */}
+        <div className={`filter_box ${isFilterOpen.brands ? 'open' : ''}`}>
+          <div className='nav_head' onClick={() => toggleFilter('brands')}>
+            <span>Brands</span>
+            <img src={arrowdown} alt='' className={isFilterOpen.brands ? 'rotate' : ''} />
+          </div>
+          <ul style={{ display: isFilterOpen.brands ? 'block' : 'none' }}>
+            {brands.map((brand) => (
+              <li key={brand.id}>{brand.model}</li>
+            ))}
+          </ul>
+          {isFilterOpen.brands && <span className='seeall'>See all</span>}
+        </div>
+
+        {/* Features Filter */}
+        <div className={`filter_box ${isFilterOpen.features ? 'open' : ''}`}>
+          <div className='nav_head' onClick={() => toggleFilter('features')}>
+            <span>Features</span>
+            <img src={arrowdown} alt='' className={isFilterOpen.features ? 'rotate' : ''} />
+          </div>
+          <ul style={{ display: isFilterOpen.features ? 'block' : 'none' }}>
+            <li>
+              <label>
+                <input type='checkbox' name='' id='' />iron
+              </label>
+            </li>
+            <li>
+              <label>
+                <input type='checkbox' name='' id='' />titanium
+              </label>
+            </li>
+            <li>
+              <label>
+                <input type='checkbox' name='' id='' />gold
+              </label>
+            </li>
+            <li>
+              <label>
+                <input type='checkbox' name='' id='' />silver
+              </label>
+            </li>
+          </ul>
+          {isFilterOpen.features && <span className='seeall'>See all</span>}
+        </div>
+
+        {/* Price Range Filter */}
+        <div className={`filter_box ${isFilterOpen.priceRange ? 'open' : ''}`}>
+          <div className='nav_head' onClick={() => toggleFilter('priceRange')}>
+            <span>Price range</span>
+            <img src={arrowdown} alt='' className={isFilterOpen.priceRange ? 'rotate' : ''} />
+          </div>
+          <div className='priserange_filter' style={{ display: isFilterOpen.priceRange ? 'block' : 'none' }}>
+            <PriceRangeSlider />
+          </div>
+        </div>
+
+        {/* Condition Filter */}
+        <div className={`filter_box ${isFilterOpen.condition ? 'open' : ''}`}>
+          <div className='nav_head' onClick={() => toggleFilter('condition')}>
+            <span>Condition</span>
+            <img src={arrowdown} alt='' className={isFilterOpen.condition ? 'rotate' : ''} />
+          </div>
+          <ul style={{ display: isFilterOpen.condition ? 'block' : 'none' }}>
+            <li>
+              <label>
+                <input type='checkbox' name='' id='' />new
+              </label>
+            </li>
+            <li>
+              <label>
+                <input type='checkbox' name='' id='' />old
+              </label>
+            </li>
+            <li>
+              <label>
+                <input type='checkbox' name='' id='' />secondary
+              </label>
+            </li>
+            <li>
+              <label>
+                <input type='checkbox' name='' id='' />feature
+              </label>
+            </li>
+          </ul>
+          {isFilterOpen.condition && <span className='seeall'>See all</span>}
+        </div>
+
+        {/* Rating Filter */}
+        <div className={`filter_box ${isFilterOpen.rating ? 'open' : ''}`}>
+          <div className='nav_head' onClick={() => toggleFilter('rating')}>
+            <span>Rating</span>
+            <img src={arrowdown} alt='' className={isFilterOpen.rating ? 'rotate' : ''} />
+          </div>
+          <ul style={{ display: isFilterOpen.rating ? 'block' : 'none' }}>
+            <li>
+              <label>
+                <input type='checkbox' name='' id='' />
+                <img src={star5} alt='' />
+              </label>
+            </li>
+            <li>
+              <label>
+                <input type='checkbox' name='' id='' />
+                <img src={star4} alt='' />
+              </label>
+            </li>
+            <li>
+              <label>
+                <input type='checkbox' name='' id='' />
+                <img src={star3} alt='' />
+              </label>
+            </li>
+            <li>
+              <label>
+                <input type='checkbox' name='' id='' />
+                <img src={star2} alt='' />
+              </label>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Productfilter;
